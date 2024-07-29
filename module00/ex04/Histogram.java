42dec70b3fc88ade7c577b725e4110c444845947
tmctrevor <mohamed.james1949@gmail.com>
2024-07-29 20:21:31 +0100

    random try

---
+++ module00/ex04/Histogram.java
@@ -0,0 +1,51 @@
+public class Histogram {
+    public static void main(String[] args) {
+        // Character frequencies
+        int[] frequencies = {36, 35, 27, 14, 12, 9, 7, 4, 2, 2};
+        String[] characters = {"D", "A", "C", "B", "F", "E", "G", "R", "T", "W"};
+
+        // Maximum number of # symbols to display
+        int maxSymbols = 10;
+
+        // Maximum frequency in the data
+        int maxFrequency = 0;
+        for (int freq : frequencies) {
+            if (freq > maxFrequency) {
+                maxFrequency = freq;
+            }
+        }
+
+        // Scale factor to adjust frequencies to maxSymbols
+        double scale = (double) maxSymbols / maxFrequency;
+
+        // Scaled frequencies
+        int[] scaledFrequencies = new int[frequencies.length];
+        for (int i = 0; i < frequencies.length; i++) {
+            scaledFrequencies[i] = (int) (frequencies[i] * scale);
+        }
+
+        // Print the frequencies on top
+        for (int freq : frequencies) {
+            System.out.printf("%2d ", freq);
+        }
+        System.out.println();
+
+        // Print the histogram vertically
+        for (int level = maxSymbols; level > 0; level--) {
+            for (int i = 0; i < scaledFrequencies.length; i++) {
+                if (scaledFrequencies[i] >= level) {
+                    System.out.print("#  ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+
+        // Print character labels
+        for (String character : characters) {
+            System.out.print(character + "  ");
+        }
+        System.out.println();
+    }
+}
