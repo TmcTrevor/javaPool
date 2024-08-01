85464ebe74f8816d14b1ca01fe3d8c560ee877e5
tmctrevor <mohamed.james1949@gmail.com>
2024-08-01 03:09:04 +0100

    ex01 is done

---
+++ module01/ex01/Program.java
@@ -0,0 +1,16 @@
+
+public class Program {
+
+    public static void main(String[] args) {
+        // Create User objects with hardcoded data
+        User alice = new User("Alice", 1000.00f);
+        UserIdsGenerator test = UserIdsGenerator.getInstance();
+        User bob = new User("Bob", 500.00f);
+
+        System.out.println("Users:");
+        System.out.println(alice);
+        System.out.println(bob);
+        System.out.println(test);
+
+    }
+}
