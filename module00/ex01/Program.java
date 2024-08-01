795f13bb8acf0d9ca0cddb17cf8c7ebbf0ea9261
tmctrevor <mohamed.james1949@gmail.com>
2024-08-01 01:10:59 +0100

    exit status for ex01

--- module00/ex01/Program.java
+++ module00/ex01/Program.java
@@ -1,38 +1,42 @@
+
 import java.util.Scanner;
 
 class Program {
 
     private static int isPrime(int number) {
-        int sqrtNumber = (int)Math.sqrt(number);
+        int sqrtNumber = (int) Math.sqrt(number);
 
-        for (int  i = 2; i <= sqrtNumber; i++) {
+        for (int i = 2; i <= sqrtNumber; i++) {
             if (number % i == 0) {
                 System.out.println("false " + (i - 1));
                 return 0;
             }
         }
-       
+
         System.out.println("true " + sqrtNumber);
         return 1;
     }
+
     public static void main(String[] args) {
-           Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         try {
-             
-            while (true)
-            {
-                 System.out.print("-> ");
-                if (!scanner.hasNextInt())
-   throw new Exception("IllegalArgument");
-            int number = scanner.nextInt();
-            if (number <= 1)
-               throw new Exception("IllegalArgument");
-            isPrime(number);
+
+            while (true) {
+                System.out.print("-> ");
+                if (!scanner.hasNextInt()) {
+                    throw new Exception("IllegalArgument");
+                }
+                int number = scanner.nextInt();
+                if (number <= 1) {
+                    throw new Exception("IllegalArgument");
+                }
+                isPrime(number);
             }
         } catch (Exception e) {
             System.err
                     .println(e.getMessage());
-                    scanner.close();
+            System.exit(-1);
+            scanner.close();
         }
     }
 }
