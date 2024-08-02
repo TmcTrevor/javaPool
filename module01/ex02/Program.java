2fb39171a8555ea5db87e46e7558e13dd5a535ad
tmctrevor <mohamed.james1949@gmail.com>
2024-08-02 20:09:04 +0100

    ex02 done

---
+++ module01/ex02/Program.java
@@ -0,0 +1,33 @@
+
+public class Program {
+
+    public static void main(String[] args) {
+        UsersArrayList userList = new UsersArrayList();
+
+        User user1 = new User("Alice", 1000.0f);
+        User user2 = new User("Bob", 500.0f);
+        User user3 = new User("Charlie", 200.0f);
+
+        userList.addUser(user1);
+        userList.addUser(user2);
+        userList.addUser(user3);
+
+        System.out.println("All users:");
+        for (int i = 0; i < userList.getNbrUsers(); i++) {
+            System.out.println(userList.getUserByIndex(i));
+        }
+
+        try {
+            User user = userList.getUserById(2);
+            System.out.println("User with ID 2: " + user);
+        } catch (UserNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            userList.getUserById(99); // This should throw an exception
+        } catch (UserNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ module01/ex02/UsersList.java
@@ -0,0 +1,6 @@
+interface UsersList {
+    public void addUser(User user);
+    User getUserById(int id) throws UserNotFoundException;
+    User getUserByIndex(int index) throws IndexOutOfBoundsException;
+    public int  getNbrUsers();
+}
