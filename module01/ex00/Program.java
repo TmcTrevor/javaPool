3e896919d9a25047f28e667998edceea392864f3
tmctrevor <mohamed.james1949@gmail.com>
2024-08-15 23:23:14 +0100

    extra example in ex00

--- module01/ex00/Program.java
+++ module01/ex00/Program.java
@@ -5,8 +5,10 @@ public class Program {
 
     public static void main(String[] args) {
         // Create User objects with hardcoded data
+		try {
         User alice = new User(1, "Alice", 1000.00f);
         User bob = new User(2, "Bob", 500.00f);
+
 
         System.out.println("Users:");
         System.out.println(alice);
@@ -18,6 +20,31 @@ public class Program {
         System.out.println("Trasactions :");
         System.out.println(transaction1);
         System.out.println(transaction2);
+		} catch (RuntimeException e)
+		{
+
+			System.err.println("Error :" + e.getMessage());
+		}
+
+
+		try {
+			User wrongUser = new User(3, "Bob", -500.00f);
+			System.out.println(wrongUser);
+		} catch (RuntimeException e)
+		{
+
+			System.err.println("Error :" + e.getMessage());
+		}
+		try {
+			User alice = new User(1, "Alice", 1000.00f);
+			User bob = new User(2, "Bob", 500.00f);
+			Transaction transaction1 = new Transaction(alice, bob, Transaction.TransferCategory.CREDITS, -200.00f);
+			System.out.println(transaction1);
+		} catch (RuntimeException e)
+		{
+
+			System.err.println("Error :" + e.getMessage());
+		}
 
     }
 }
