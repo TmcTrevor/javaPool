06e5d3f5f6bf46c9870b1bfa606378938bd9dfee
tmctrevor <mohamed.james1949@gmail.com>
2024-08-20 19:18:10 +0100

    update m3.ex00 main

--- module03/ex00/Program.java
+++ module03/ex00/Program.java
@@ -1,32 +1,37 @@
 package module03.ex00;
-
-public class Program extends Thread {
-	public static int count = 0;
+public class Program {
 	public static void main(String[] args) {
-		if (args.length == 1)
-		{
-		count = Integer.parseInt(args[0].split("=")[1]);
-		Egg thread = new Egg(count);
-		Hen henObj = new Hen(count);
-
-		Thread thread1 = new Thread(henObj);
-		thread1.start();
-		thread.start();
-
-		while(thread.isAlive() && thread1.isAlive()) {
-
+		if (args.length != 1) {
+			System.err.println("Error: wrong number of arguments");
+			System.exit(-1);
 		}
 
-
-	  }
-
-	  @Override
-	 public  void run() {
-		  for (int i = 0; i < count; i++)
-		  System.out.println("HUman");
+		String[] input = args[0].split("=");
+		if (!input[0].equals("--count")) {
+			System.err.println("Error: Wrong flag");
+			System.exit(-1);
 		}
-}
 
+		try {
+			int count = Integer.parseInt(input[1]);
 
+			Egg eggThread = new Egg(count);
+			Hen henObj = new Hen(count);
 
-}
+			Thread henThread = new Thread(henObj);
+
+			eggThread.start();
+			henThread.start();
+
+			eggThread.join();
+			henThread.join();
+
+			for (int i = 0; i < count; i++) {
+				System.out.println("Human");
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			System.exit(-1);
+		}
+	}
+};
