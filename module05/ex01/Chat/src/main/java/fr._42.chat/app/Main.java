26ae47f7ddcee48e091a6a00404f4d0cb725f2fc
tmctrevor <mohamed.james1949@gmail.com>
2024-09-08 23:33:34 +0100

    m5 ex01 done -printing

---
+++ module05/ex01/Chat/src/main/java/fr._42.chat/app/Main.java
@@ -0,0 +1,130 @@
+package fr._42.chat.app;
+// This code is for establishing connection with MySQL
+// database and retrieving data
+// from db Java Database connectivity
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Optional;
+import java.util.Scanner;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import fr._42.chat.models.Message;
+import fr._42.chat.repositories.MessageRepositoryJdbcImpl;
+
+public class Main {
+
+    public static void listUsers(Statement st) {
+        try {
+            ResultSet rs = st.executeQuery("SELECT * FROM \"User\"");
+            System.err.println("Users:");
+            while (rs.next()) {
+                System.out.println(rs.getString("login"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+    }
+    }
+
+    public static void listChatrooms(Statement st) {
+        try {
+            ResultSet rs = st.executeQuery("SELECT * FROM \"Chatroom\"");
+            System.err.println("Chatrooms:");
+            while (rs.next()) {
+                System.out.println(rs.getString("roomName"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void listMessages(Statement st) {
+        try {
+            ResultSet rs = st.executeQuery("SELECT * FROM \"Message\"");
+            System.err.println("Messages:");
+            while (rs.next()) {
+                System.out.println(rs.getString("text") +  " by " + rs.getString("author") + " in Room : " + rs.getString("room"));
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+	public static void main(String[] args) throws Exception
+	{
+        Connection connection = null;
+        try {
+
+            HikariConfig config = new HikariConfig();
+config.setJdbcUrl("jdbc:postgresql://localhost:5432/testdb");
+config.setUsername("trevor");
+config.setPassword("trevor123@");
+config.addDataSourceProperty("cachePrepStmts", "true");
+config.addDataSourceProperty("prepStmtCacheSize", "250");
+config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+HikariDataSource ds = new HikariDataSource(config);
+            // Dotenv dotenv = Dotenv.load();
+
+            MessageRepositoryJdbcImpl messageRepositoryJdbcImpl = new MessageRepositoryJdbcImpl(ds);
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a message ID");
+            System.out.print("-> ");
+            long id = scanner.nextLong();
+           Optional<Message> message =  messageRepositoryJdbcImpl.findById(id);
+           System.out.println(message.isPresent() ? message.get().getText() : "");
+            // // Access the environment variables
+            // String dbPassword = dotenv.get("POSTGRES_PASSWORD");
+            // String dbUser = dotenv.get("POSTGRES_USER");
+            // String dbName = dotenv.get("POSTGRES_DB");
+//		String url
+//			= "jdbc:postgresql://localhost:5432/testdb"; // table details
+//		String username = "trevor"; // MySQL credentials
+//		String password = "trevor123@";
+//		String query
+//			= "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"; // query to be run
+//        Class.forName("org.postgresql.Driver");// Driver name
+//		// connection = DriverManager.getConnection(url, username, password);
+//
+//        connection = ds.getConnection();
+//        if (connection != null) {
+//            System.out.println("Connected to the database!");
+//        } else {
+//            System.out.println("Failed to make connection!");
+//        }
+//		Statement st = connection.createStatement();
+//		ResultSet rs
+//			= st.executeQuery(query); // Execute query
+//		// rs.next();
+//        while (rs.next()) {
+//            String tableName = rs.getString("table_name");
+//            System.out.println(tableName);
+//        }
+//        listUsers(st);
+//        listChatrooms(st);
+//        listMessages(st);
+//
+//        st.close();
+		// String name
+		// 	= rs.getString("login"); // Retrieve name from db
+        // System.out.println(name); // Print result on console
+    } catch (Exception e) {
+        System.out.println("Connection failure.");
+        e.printStackTrace();
+    } finally {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+            // st.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+		// System.out.println(name); // Print result on console
+		 // close statement
+//		connection.close(); // close connection
+		System.out.println("Connection Closed....");
+	}
+}
---
+++ module05/ex01/Chat/src/main/java/fr._42.chat/models/Message.java
@@ -0,0 +1,80 @@
+package fr._42.chat.models;
+
+import java.util.Date;
+
+
+
+
+public class Message {
+    private int id;
+    private String text;
+    private final User author;
+    private final Chatroom room;
+    private final Date datetime;
+
+
+    public Message(int id, User author, Chatroom room, String text, Date datetime) {
+        this.id = id;
+        this.author = author;
+        this.room = room;
+        this.text = text;
+        this.datetime = datetime;
+    }
+    public Message(User author, Chatroom room, String text) {
+        this.author = author;
+        this.room = room;
+        this.text = text;
+        this.datetime = new Date();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public Chatroom getRoom() {
+        return room;
+    }
+
+    public Date getDatetime() {
+        return datetime;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Message) {
+            return ((Message) obj).id == this.id;
+        }
+        return false;
+    }
+    
+    // @Override
+    // public int hashCode() {
+    //     return Object.hash(id, text, author, room, datetime);
+    // }
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * author.hashCode();
+        result = 31 * room.hashCode();
+        return result;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Message from " + author + " in room " + room + " at " + datetime + " : " + text;
+    }
+
+}
